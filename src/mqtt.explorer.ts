681b19a75e30ec956ed9f878f0cc08028313e4a7
microud <gmicroud@gmail.com>
2020-04-08 22:36:48 +0800

    feat: refactor subscriber loader code

--- src/mqtt.explorer.ts
+++ src/mqtt.explorer.ts
@@ -2,21 +2,23 @@ import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { DiscoveryService, MetadataScanner, Reflector } from '@nestjs/core';
 import { InstanceWrapper } from '@nestjs/core/injector/instance-wrapper';
 import {
-  MQTT_CLIENT_INSTANCE,
-  MQTT_SUBSCRIBE_TOPIC,
+  MQTT_CLIENT_INSTANCE, MQTT_OPTION_PROVIDER,
+  MQTT_SUBSCRIBE_OPTIONS,
   MQTT_SUBSCRIBER_PARAMS,
 } from './mqtt.constants';
 import { Client } from 'mqtt';
 import { Packet } from 'mqtt-packet';
 import { getTransform } from './mqtt.transform';
 import {
-  IMqttSubscriber,
-  IMqttSubscriberParameter,
+  MqttModuleOptions,
+  MqttSubscribeOptions,
+  MqttSubscriber,
+  MqttSubscriberParameter,
 } from './mqtt.interface';
 
 @Injectable()
 export class MqttExplorer implements OnModuleInit {
-  subscribers: IMqttSubscriber[];
+  subscribers: MqttSubscriber[];
 
   constructor(
     private readonly discoveryService: DiscoveryService,
@@ -24,6 +26,7 @@ export class MqttExplorer implements OnModuleInit {
     private readonly logger: Logger,
     private readonly reflector: Reflector,
     @Inject(MQTT_CLIENT_INSTANCE) private readonly client: Client,
+    @Inject(MQTT_OPTION_PROVIDER) private readonly options: MqttModuleOptions,
   ) {
     this.subscribers = [];
   }
@@ -31,6 +34,51 @@ export class MqttExplorer implements OnModuleInit {
   onModuleInit() {
     this.logger.log('MqttModule dependencies initialized');
     this.explore();
+  }
+
+  preprocess(options: MqttSubscribeOptions): string | string[] {
+    const processTopic = (topic) => {
+      const queue = typeof options.queue === 'boolean' ? options.queue : this.options.queue;
+      const shared = typeof options.shared === 'string' ? options.shared : this.options.shared;
+      topic = topic.replace('$queue/', '')
+        .replace(/^\$share\/([A-Za-z0-9]+)\//, '');
+      if (queue) {
+        return `$queue/${topic}`;
+      }
+
+      if (shared) {
+        return `$shared/${shared}/${topic}`;
+      }
+    };
+    if (Array.isArray(options.topic)) {
+      return options.topic.map(processTopic);
+    } else {
+      return processTopic(options.topic);
+    }
+  }
+
+  subscribe(options: MqttSubscribeOptions, parameters: MqttSubscriberParameter[], handle) {
+    this.client.subscribe(this.preprocess(options), err => {
+      if (!err) {
+        // put it into this.subscribers;
+        (Array.isArray(options.topic) ? options.topic : [options.topic])
+          .forEach(topic => {
+            this.subscribers.push({
+              topic,
+              route: topic.replace('$queue/', '')
+                .replace(/^\$share\/([A-Za-z0-9]+)\//, ''),
+              regexp: MqttExplorer.topicToRegexp(topic),
+              handle,
+              options,
+              parameters,
+            });
+          });
+      } else {
+        this.logger.error(
+          `subscribe topic [${options.topic} failed]`,
+        );
+      }
+    });
   }
 
   explore() {
@@ -44,34 +92,16 @@ export class MqttExplorer implements OnModuleInit {
         instance,
         Object.getPrototypeOf(instance),
         key => {
-          const topicDetail = this.reflector.get(
-            MQTT_SUBSCRIBE_TOPIC,
+          const subscribeOptions: MqttSubscribeOptions = this.reflector.get(
+            MQTT_SUBSCRIBE_OPTIONS,
             instance[key],
           );
           const parameters = this.reflector.get(
             MQTT_SUBSCRIBER_PARAMS,
             instance[key],
           );
-          if (topicDetail) {
-            this.client.subscribe(topicDetail.topic, err => {
-              if (!err) {
-                const topic =
-                  topicDetail.topic.indexOf('$queue') === 0
-                    ? topicDetail.topic.substr(7)
-                    : topicDetail.topic;
-                this.subscribers.push({
-                  topic,
-                  regexp: MqttExplorer.topicToRegexp(topic),
-                  handle: instance[key],
-                  metadata: topicDetail,
-                  parameters,
-                });
-              } else {
-                this.logger.error(
-                  `subscribe topic [${topicDetail.topic} failed]`,
-                );
-              }
-            });
+          if (subscribeOptions) {
+            this.subscribe(subscribeOptions, parameters, instance[key]);
           }
         },
       );
@@ -82,11 +112,11 @@ export class MqttExplorer implements OnModuleInit {
         const subscriber = this.getSubscriber(topic);
         if (subscriber) {
           const parameters = subscriber.parameters || [];
-          const scatterParameters: IMqttSubscriberParameter[] = [];
+          const scatterParameters: MqttSubscriberParameter[] = [];
           for (const parameter of parameters) {
             scatterParameters[parameter.index] = parameter;
           }
-          const transform = getTransform(subscriber.metadata.transform);
+          const transform = getTransform(subscriber.options.transform);
           subscriber.handle(
             ...scatterParameters.map(parameter => {
               switch (parameter?.type) {
@@ -108,8 +138,9 @@ export class MqttExplorer implements OnModuleInit {
     );
   }
 
-  private getSubscriber(topic: string): IMqttSubscriber | null {
+  private getSubscriber(topic: string): MqttSubscriber | null {
     for (const subscriber of this.subscribers) {
+      subscriber.regexp.lastIndex = 0;
       if (subscriber.regexp.test(topic)) {
         return subscriber;
       }
@@ -118,26 +149,26 @@ export class MqttExplorer implements OnModuleInit {
   }
 
   private static topicToRegexp(topic: string) {
-    // emqtt兼容，共享订阅
+    // compatible with emqtt
     return new RegExp(
       '^' +
-        topic
-          .replace('$queue/', '')
-          .replace(/^\$share\/([A-Za-z0-9]+)\//, '')
-          .replace(/([\[\]\?\(\)\\\\$\^\*\.|])/g, '\\$1')
-          .replace(/\+/g, '([^/]+)')
-          .replace(/\/#$/, '(/.*)?') +
-        '$',
+      topic
+        .replace('$queue/', '')
+        .replace(/^\$share\/([A-Za-z0-9]+)\//, '')
+        .replace(/([\[\]\?\(\)\\\\$\^\*\.|])/g, '\\$1')
+        .replace(/\+/g, '([^/]+)')
+        .replace(/\/#$/, '(/.*)?') +
+      '$',
       'y',
     );
   }
 
   private static matchGroups(str: string, regex: RegExp) {
     regex.lastIndex = 0;
-    let m;
+    let m = regex.exec(str);
     const matches: string[] = [];
 
-    while ((m = regex.exec(str)) !== null) {
+    while (m !== null) {
       // This is necessary to avoid infinite loops with zero-width matches
       if (m.index === regex.lastIndex) {
         regex.lastIndex++;
@@ -145,10 +176,11 @@ export class MqttExplorer implements OnModuleInit {
 
       // The result can be accessed through the `m`-variable.
       m.forEach((match, groupIndex) => {
-        if (groupIndex === 1) {
+        if (groupIndex !== 0) {
           matches.push(match);
         }
       });
+      m = regex.exec(str);
     }
     return matches;
   }
