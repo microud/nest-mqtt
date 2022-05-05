ea08dfac8d5c1e43b2c5ee9d55ae4ea1d28e0c62
acodercat <codercat@163.com>
2022-05-05 16:23:43 +0800

    Remove JSON.stringify.

--- src/mqtt.service.ts
+++ src/mqtt.service.ts
@@ -34,9 +34,6 @@ export class MqttService {
 
   publish(topic: string, message: string | Buffer | object, opts?: IClientPublishOptions): Promise<Packet> {
     return new Promise<Packet>((resolve, reject) => {
-      if (typeof message === 'object') {
-        message = JSON.stringify(message);
-      }
       this.client.publish(topic, message, opts || null, (error, packet) => {
         if (error) {
           reject(error);
